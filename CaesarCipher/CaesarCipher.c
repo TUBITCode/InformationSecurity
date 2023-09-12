7876660e22c2848ceb10b7783f9e9b829c916ed7
N11rm44L 7w44711 <60023424+niirmaaltwaatii@users.noreply.github.com>
2023-09-13 04:55:54 +0545

    code reduced significantely
    wrapped encrypt function making usable for decrypt too & some white spaces removed

--- CaesarCipher/CaesarCipher.c
+++ CaesarCipher/CaesarCipher.c
@@ -1,69 +1,43 @@
-#include<stdio.h>
-#include<ctype.h>
-
+#include <stdio.h>
+#include <ctype.h>
+void caesar_encrypt(char text[], int key){
+    char ch;
+    for(int i = 0; text[i] != '\0'; ++i){
+        ch = text[i];
+        if (isalnum(ch)){
+            if (islower(ch))
+                ch = (ch - 'a' + key+26) % 26 + 'a';
+            if (isupper(ch))
+                ch = (ch - 'A' + key+26) % 26 + 'A';
+            if (isdigit(ch))
+                ch = (ch - '0' + key+50) % 10 + '0';
+        }
+        text[i] = ch;
+    }
+}
 int main() {
-    char text[100], ch;
-    int key,l=1,op;
-    printf("[----------- Caeser Cipher ------------]\n");
+    char text[100];
+    int key, l = 1, op;   
+    printf("[----------- Caesar Cipher ------------]\n");
     printf("\t1] Encrypt\n\t2] Decrypt\n\t0] Exit\n");
-    while(l){
-    	printf("[Option]=> ");
-    	scanf("%d",&op);
-    	if(op==1){
-    		printf("\tPlainText : ");
-		    scanf("%s", text);
-		    printf("\tKey: ");
-		    scanf("%d", & key);
-		    
-		    for (int i = 0; text[i] != '\0'; ++i) {
-		        ch = text[i];
-		        if (isalnum(ch)) {
-		            if (islower(ch)) {
-		                ch = (ch - 'a' + key) % 26 + 'a';
-		            }
-		            if (isupper(ch)) {
-		                ch = (ch - 'A' + key) % 26 + 'A';
-		            }
-		            if (isdigit(ch)) {
-		                ch = (ch - '0' + key) % 10 + '0';
-		            }
-		        }
-		        else {
-		            printf("\tInvalid Character!");
-		        }
-		        text[i] = ch;
-		    }
-		    printf("Encrypted Text: %s\n", text);
-		}else if(op==2){
-			printf("\tText to Decrypt: ");
-		    scanf("%s", text);
-			printf("\tKey: ");
-		    scanf("%d", & key);
-		
-		    for (int i = 0; text[i] != '\0'; ++i) {
-		        ch = text[i];
-		        if (isalnum(ch)) {
-		            if (islower(ch)) {
-		                ch = (ch - 'a' - key + 26) % 26 + 'a';
-		            }
-		            if (isupper(ch)) {
-		                ch = (ch - 'A' - key + 26) % 26 + 'A';
-		            }
-		            if (isdigit(ch)) {
-		                ch = (ch - '0' - key + 10) % 10 + '0';
-		            }
-		        }
-		        else {
-		            printf("Invalid Character!");
-		        }
-		        text[i] = ch;
-		    }
-		    printf("Decrypted Text: %s\n", text);		
-		}else if(op==0){
-			l=0;
-		}else{
-			printf("\tInvalid Option !\n");
-		}
-	}
-	return 0;
+    while (l){
+        printf("[Option]=> ");
+        scanf("%d", &op);
+        if(op == 1 || op == 2){
+            printf("\tEnter Text: ");
+            scanf("%s", text);
+            printf("\tKey: ");
+            scanf("%d", &key);
+            if (op == 2) {
+                key = -key;
+            }
+            caesar_encrypt(text, key);
+            printf("%s Text: %s\n", (op == 1) ? "Encrypted" : "Decrypted", text);
+        }else if (op == 0){
+            l = 0;
+        }else{
+            printf("\tInvalid Option !\n");
+        }
+    }
+    return 0;
 }
