fd0b21eb9ba46616ddf41ebf59598b9e878970b4
N11rm44L 7w44711 <60023424+niirmaaltwaatii@users.noreply.github.com>
2023-09-19 01:39:54 +0545

    Update RailFenceCipher.c
    plaintext,key,ciphertext : user input

--- RailFenceCipher/RailFenceCipher.c
+++ RailFenceCipher/RailFenceCipher.c
@@ -1,57 +1,61 @@
-#include<stdio.h>
-#include<string.h>
-void encryptMsg(char msg[], int key){
+#include <stdio.h>
+#include <string.h>
+void encryptMsg(char msg[], int key) {
     int msgLen = strlen(msg), i, j, k = -1, row = 0, col = 0;
     char railMatrix[key][msgLen];
-    for(i = 0; i < key; ++i)
-        for(j = 0; j < msgLen; ++j)
+    for (i = 0; i < key; ++i)
+        for (j = 0; j < msgLen; ++j)
             railMatrix[i][j] = '\n';
-    for(i = 0; i < msgLen; ++i){
+    for (i = 0; i < msgLen; ++i) {
         railMatrix[row][col++] = msg[i];
-        if(row == 0 ||row == key-1)
-            k= k * (-1);
+        if (row == 0 || row == key - 1)
+            k = k * (-1);
         row = row + k;
     } 
-    printf("\nEncrypted Text: ");
-    for(i = 0; i < key; ++i)
-        for(j = 0; j < msgLen; ++j)
-            if(railMatrix[i][j] != '\n')
+    printf("Encrypted Text: ");
+    for (i = 0; i < key; ++i)
+        for (j = 0; j < msgLen; ++j)
+            if (railMatrix[i][j] != '\n')
                 printf("%c", railMatrix[i][j]);
 }
-void decryptMsg(char enMsg[], int key){
+void decryptMsg(char enMsg[], int key) {
     int msgLen = strlen(enMsg), i, j, k = -1, row = 0, col = 0, m = 0;
     char railMatrix[key][msgLen];
-    for(i = 0; i < key; ++i)
-        for(j = 0; j < msgLen; ++j)
+    for (i = 0; i < key; ++i)
+        for (j = 0; j < msgLen; ++j)
             railMatrix[i][j] = '\n';
-    for(i = 0; i < msgLen; ++i){
+    for (i = 0; i < msgLen; ++i) {
         railMatrix[row][col++] = '*';
-        if(row == 0 || row == key-1)
-            k= k * (-1);
+        if (row == 0 || row == key - 1)
+            k = k * (-1);
         row = row + k;
     }
-    for(i = 0; i < key; ++i)
-        for(j = 0; j < msgLen; ++j)
-            if(railMatrix[i][j] == '*')
-                railMatrix[i][j] = enMsg[m++];
+    for (i = 0; i < key; ++i)
+        for (j = 0; j < msgLen; ++j)
+            if (railMatrix[i][j] == '*')
+                railMatrix[i][j] = enMsg[m++];   
     row = col = 0;
     k = -1;
-    printf("\nDecrypted Text: ");
-    for(i = 0; i < msgLen; ++i){
+    printf("Decrypted Text: ");
+    for (i = 0; i < msgLen; ++i) {
         printf("%c", railMatrix[row][col++]);
-        if(row == 0 || row == key-1)
-            k= k * (-1);
+        if (row == 0 || row == key - 1)
+            k = k * (-1);
         row = row + k;
     }
 }
-int main(){
-    char msg[] = "Hello World";
-    char enMsg[] = "Horel ollWd";
-    int key = 3;
- 
-    printf("Plaintext: %s", msg);
-	printf("\nKey: %d",key);
+int main() {
+    char msg[100];
+    int key;
+    printf("Enter the plaintext: ");
+    scanf("%s", msg);
+    printf("Enter the key(number of rails): ");
+    scanf("%d", &key);
     encryptMsg(msg, key);
-    decryptMsg(enMsg, key);
+    printf("\nEnter the ciphertext for decryption: ");
+    scanf("%s", msg);
+    printf("Enter the key(number of rails): ");
+    scanf("%d", &key);
+    decryptMsg(msg, key);
     return 0;
 }
